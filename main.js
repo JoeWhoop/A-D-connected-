ea15396e13c2005684d0a4b632960191d16a3296
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2023-11-11 10:28:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ document.getElementById("liniaDropdown").addEventListener("change", function () 
     // Populate mechanic dropdown based on the selected line
     var mechanics = (selectedLine === "A") ? mechanicsA : mechanicsD;
 
+    // Populate mechanic dropdown
     mechanics.forEach(function (mechanic) {
         var option = document.createElement("option");
         option.value = mechanic.replace(" ", "_").toUpperCase();
@@ -33,45 +34,4 @@ document.getElementById("goButton").addEventListener("click", function () {
         ? "https://web.miniextensions.com/c9PrY0xXNNhsDJQQeLCL?preffill_Mechanic="
         : "https://web.miniextensions.com/Bwk6PtCv1a4zbRONadUU?prefill_Mechanic=";
 
-    var redirectionURL = baseRedirectURL + selectedMechanic + "&prefill_Station=" + selectedStation;
-
-    if (selectedMechanic && selectedStation) {
-        window.location.href = redirectionURL;
-    } else {
-        alert("Please select both a mechanic and a station before clicking Go.");
-    }
-});
-
-// Function to populate "Znajdz siebie" dropdown based on the selected line
-function populateMechanicDropdown(selectedLine) {
-    var mechanicDropdown = document.getElementById("mechanicDropdown");
-    var mechanics = [];
-
-    // Mechanics for A line
-    if (selectedLine === "A") {
-        mechanics = ["Adam Radzikowski", "Anton Pratasau", "Bartosz Płoszaj", "Dominik Chojnowski", "Maciej Chwil", "Szymon Niezgoda", "Szymon Staniszewski", "Wiktor Kaczkowski"];
-    }
-    // Mechanics for D line
-    else if (selectedLine === "D") {
-        mechanics = ["Aleks Czarnecki", "Bartosz Kędra", "Hubert Jakubowski", "Kacper Wydra", "Łukasz Chojecki", "Mateusz Wiatr", "Stanislav Tymchenko", "Weronika Szałowska", "Sebastian Widuliński", "Łukasz Markowski", "Kacper Maciejczuk", "Maciej Stępniak"];
-    }
-
-    // Clear existing options
-    mechanicDropdown.innerHTML = "";
-
-    // Populate mechanic dropdown
-    mechanics.forEach(function (mechanic) {
-        var option = document.createElement("option");
-        option.value = mechanic.replace(" ", "_").toUpperCase();
-        option.label = mechanic;
-        mechanicDropdown.appendChild(option);
-    });
-}
-
-// Initial population of "Znajdz siebie" dropdown
-populateMechanicDropdown(document.getElementById("liniaDropdown").value);
-
-// Update the "Znajdz siebie" dropdown when the line changes
-document.getElementById("liniaDropdown").addEventListener("change", function () {
-    populateMechanicDropdown(document.getElementById("liniaDropdown").value);
-});
+    var redirectionURL = baseRedirectURL + selectedMechanic + "&prefill_
